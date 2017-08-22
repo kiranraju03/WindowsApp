796e64f981f082d0fa993b514fc32cab2d678883
kiranraju03 <kiran.raju.k.43@gmail.com>
2017-08-22 15:15:12 +0530

    Initial windows application model details

---
+++ script.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+window = Tk() #creates a window
+
+b1 = Button(window, text="run") #creates a button
+b1.grid(row=0,column=0) # places the button on the window
+
+e1 = Entry(window) #creates an entry form
+e1.grid(row=0,column=1) # places the entry space on the window
+
+t1 = Text(window,height=1,width=20) #creates an text area of specified height and width.
+t1.grid(row=0,column=2) # places the text area on the window
+
+window.mainloop() #keeps the window open until user closes it, or else window will close soon.
